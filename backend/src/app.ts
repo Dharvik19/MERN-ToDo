3ebd2a79dd3ee2923ef017ece6f4d825e4ca40f8
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-19 17:04:59 +0530

    mongoDB model setup for todos

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,28 @@
 import "dotenv/config";
-import express from 'express'
-
+import express,{NextFunction, Request, Response} from 'express'
+import todoModel from './models/todo';
 const app = express();
-app.get("/",(req, res)=>{
-    res.send("Hello World");
+app.get("/",async (req, res, next)=>{
+    try{
+        // throw Error("Bazinga")
+        const todos  = await todoModel.find().exec();
+        res.status(200).json(todos);
+    }catch(error){
+        next(error);
+    }
+    
+})
+
+app.use((req, res, next)=>{
+    next(Error("EndPoint not found"));
+})
+
+app.use((error : unknown, req : Request, res : Response, next : NextFunction)=>{
+    console.error(error);
+    let errorMessage = "an unknown error occured";
+    if(error instanceof Error) errorMessage = error.message;
+    res.status(500).json({error : errorMessage})
+    console.log(next);
 })
 
 export default app;
