b433f8ba64e76e21995097c92dfd1bf140e4b60f
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-19 19:02:23 +0530

    Better error handling with status codes & messages

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express,{NextFunction, Request, Response} from 'express';
 import todoRoutes from './routes/todos';
 import morgan from 'morgan';
+import createHttpError, { isHttpError } from "http-errors";
 const app = express();
 
 app.use(morgan("dev"));
@@ -11,15 +12,19 @@ app.use(express.json());
 app.use("/api/todos", todoRoutes);
 
 app.use((req, res, next)=>{
-    next(Error("EndPoint not found"));
+    next(createHttpError(404, "EndPoint not found"));
 })
 
-app.use((error : unknown, req : Request, res : Response, next : NextFunction)=>{
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
-    let errorMessage = "an unknown error occured";
-    if(error instanceof Error) errorMessage = error.message;
-    res.status(500).json({error : errorMessage})
-    console.log(next);
-})
+    let errorMessage = "An unknown error occurred";
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
 
 export default app;
--- backend/src/controllers/todos.ts
+++ backend/src/controllers/todos.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import todoModel from '../models/todo'
-
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 export const getTodos : RequestHandler = async (req , res, next)=>{
     try{
         const todos  = await todoModel.find().exec();
@@ -14,18 +15,35 @@ export const getTodo : RequestHandler = async (req, res, next)=>{
     const todoId = req.params.todoId;
 
     try{
+        if (!mongoose.isValidObjectId(todoId)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
         const todo  = await todoModel.findById(todoId).exec();
+        if(!todo){
+            throw createHttpError(404, "Note not found");
+        }
         res.status(200).json(todo);
     }catch(error){
         next(error);
     }
 }
-export const createTodos : RequestHandler = async (req, res, next)=>{
+
+interface createTodoBody {
+    title? : string,
+    text? : string,
+    isCompleted? : boolean
+}
+
+export const createTodos : RequestHandler<unknown, unknown, createTodoBody, unknown> = async (req, res, next)=>{
     const title = req.body.title;
     const text  = req.body.text;
     const isCompleted = req.body.isCompleted;
 
     try{
+        if (!title) {
+            throw createHttpError(400, "Note must have a title");
+        }
         const newTodo = await todoModel.create({
             title : title,
             text : text,
