caeeadcfa5306239d066f4d870a70be5b7c45063
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-19 16:16:18 +0530

    Successfully connected to mongodb databse

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
-import express from 'express'
+import app from './app';
+import env from './util/validateEnv';
+import mongoose from "mongoose";
 
-const app = express();
 
-const port = 5000;
 
-app.get("/",(req, res)=>{
-    res.send("Hello World");
-})
+const port  = env.PORT;
 
-app.listen(port,()=>{
-    console.log("server running on " + port)
-})
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+.then(()=>{
+    console.log("mongoose connected");
+    app.listen(port, ()=>{
+        console.log("Server running on port: " + port);
+    })
+}).catch(console.error);
