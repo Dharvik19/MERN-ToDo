92ea1cfdf05b48bd118b6175653a47c38200a7b9
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-19 21:47:09 +0530

    Fetching todos from the server for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Todo as TodoModel } from './models/todo';
 
 function App() {
+
+  const [todos, setTodos] = useState<TodoModel[]>([]);
+
+  useEffect(()=>{
+
+    async function loadNotes(){
+      try{
+        const response = await fetch("/api/todos", {method : "GET"});
+        const todos = await response.json();
+        setTodos(todos);
+      }catch(error){
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Front End!
+      </h1>
+        {JSON.stringify(todos)}
     </div>
   );
 }
