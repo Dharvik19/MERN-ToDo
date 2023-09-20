f1bd7c31227c6a182bb975e72764b9231adf3038
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-20 13:41:05 +0530

    User Authentication from backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,32 @@
 import "dotenv/config";
 import express,{NextFunction, Request, Response} from 'express';
 import todoRoutes from './routes/todos';
+import userRoutes from './routes/user';
 import morgan from 'morgan';
 import createHttpError, { isHttpError } from "http-errors";
+import session from 'express-session';
+import env from './util/validateEnv';
+import MongoStore from "connect-mongo";
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use(session({
+    secret : env.SESSION_SECRET,
+    resave : false,
+    saveUninitialized : false,
+    cookie : {
+        maxAge : 60 * 60 * 1000,
+    },
+    rolling : true,
+    store : MongoStore.create({
+        mongoUrl : env.MONGO_CONNECTION_STRING
+    })
+}))
+
+app.use("/api/users", userRoutes);
 app.use("/api/todos", todoRoutes);
 
 app.use((req, res, next)=>{
