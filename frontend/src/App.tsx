93ac96b7cca1f0f3e61269a67bd29c165c1efccb
Dharvik19 <dharvik.badga22@gmail.com>
2023-09-20 10:50:25 +0530

    Added Loading and Error states for todos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
 import { Todo as TodoModel } from "./models/todo";
 import Todo from "./components/Todo";
 import styles from "./styles/TodoPage.module.css";
@@ -9,17 +9,38 @@ import AddEditTodoDialog from "./components/AddEditTodoDialog";
 import {FaPlus} from 'react-icons/fa';
 function App() {
   const [todos, setTodos] = useState<TodoModel[]>([]);
-
+  const [todosLoading, setTodosLoading] = useState(true);
+  const [showTodsLoadingEror, setShowTodsLoadingEror] = useState(false);
+   
   const [showAddTodoDialog, setShowAddTodoDialog] = useState(false);
   const [todoToEdit,setTodoToEdit ] = useState<TodoModel | null>(null);
+
+  const todosGrid = 
+  <Row xs={1} md={2} xl={3} className={`${styles.todosGrid} g-4`}>
+  {todos.map((todo) => (
+    <Col>
+      <Todo 
+      todo={todo} 
+      className={styles.todo} 
+      onToDoClicked={setTodoToEdit}
+      onDeleteTodoClicked={deleteTodo}
+      key={todo._id} />
+    </Col>
+  ))}
+</Row>
   useEffect(() => {
     async function loadNotes() {
       try {
+        setShowTodsLoadingEror(false);
+        setTodosLoading(true);
         const todos = await TodosApi.fetchNotes();
         setTodos(todos);
       } catch (error) {
         console.error(error);
-        alert(error);
+        setShowTodsLoadingEror(true);
+
+      }finally{
+        setTodosLoading(false);
       }
     }
     loadNotes();
@@ -35,24 +56,23 @@ function App() {
     }
   }
   return (
-    <Container>
+    <Container className={styles.todosPage}>
       <h1>TO-Dos</h1>
       <Button className={`${styleUtils.blockCenter} ${styleUtils.flexCenter} mb-4`}onClick={() => setShowAddTodoDialog(true)}>
         <FaPlus/>
         Add Note
         </Button>
-      <Row xs={1} md={2} xl={3} className="g-4">
-        {todos.map((todo) => (
-          <Col>
-            <Todo 
-            todo={todo} 
-            className={styles.todo} 
-            onToDoClicked={setTodoToEdit}
-            onDeleteTodoClicked={deleteTodo}
-            key={todo._id} />
-          </Col>
-        ))}
-      </Row>
+      {todosLoading && <Spinner animation='border' variant='primary'/>}
+
+      {showTodsLoadingEror && <p>Something went wrong please refresh the page</p>}
+
+      {!todosLoading && !showTodsLoadingEror && 
+        <>
+          {
+            todos.length > 0 ? todosGrid : <p>You don't have any todos yet</p>
+          }
+        </>
+      }
       {showAddTodoDialog && (
         <AddEditTodoDialog
           onDismiss={() => setShowAddTodoDialog(false)}
